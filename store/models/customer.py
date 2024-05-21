3a68f2d75ea1bd16f2cf1f5933ff5dc5c2f94093
marielean <elena01.mari@edu.unife.it>
2024-05-21 11:03:08 +0200

    changed attributes for customer

--- store/models/customer.py
+++ store/models/customer.py
@@ -3,7 +3,6 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     password = models.CharField(max_length=255)
