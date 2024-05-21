ba72d777f45cc84156df4658db753a4a77d30964
marielean <elena01.mari@edu.unife.it>
2024-05-21 10:55:07 +0200

    add views

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,15 @@
+from django.contrib import admin
 from django.urls import path
-
 from .views import *
+from .middlewares.auth import auth_middleware
 
 urlpatterns = [
-    path("", Index.as_view(), name="index"),
+    path("", Index.as_view(), name="homepage"),
+    path('store', store, name='store'),
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout, name='logout'),
+    path('cart', auth_middleware(Cart.as_view()), name='cart'),
+    path('checkout', CheckOut.as_view(), name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders')
 ]
