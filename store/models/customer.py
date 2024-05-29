4f143b58cb2b3a8614f2b139b3f35c7813246077
marielean <elena01.mari@edu.unife.it>
2024-05-29 22:27:26 +0200

    Fix missed fields in customer (custom user). They caused some bugs in superuser creation

--- store/models/customer.py
+++ store/models/customer.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 
-class CustomerManager(BaseUserManager):
+class CustomerManager(UserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
@@ -10,6 +10,16 @@ class CustomerManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
 
 class Customer(AbstractBaseUser):
     first_name = models.CharField(max_length=255)
@@ -17,11 +27,21 @@ class Customer(AbstractBaseUser):
     phone = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    # The following fields are required for Django to work with the User model to create superusers
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
 
     objects = CustomerManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'password']
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
