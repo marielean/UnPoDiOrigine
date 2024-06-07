9c706712d3d0bfcebe562f1d18032a8cd6da4ae5
marielean <elena01.mari@edu.unife.it>
2024-06-07 19:51:01 +0200

    Update docker files to add volume data instead of db.sqlite3

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 .venv/bin/python manage.py makemigrations
 .venv/bin/python manage.py migrate
 
-if [ ! -f db.sqlite3 ]; then
+if [ ! -f data/db.sqlite3 ]; then
     venv/bin/python manage.py createsuperuser --noinput --email admin@email.com --password admin --first_name Admin --last_name Default --phone 0000000000
 fi
 
